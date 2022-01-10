d5d6960d7924756c33f72ef455e0a0aaf94abcd9
guilhermefb <guilherme.cipriano@ccc.ufcg.edu.br>
2022-01-10 01:58:56 -0300

    Add basic implementation of notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:d_brain_test/src/app_module.dart';
 import 'package:d_brain_test/src/app_widget.dart';
+import 'package:d_brain_test/src/shared/notifications/main_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+
+
 void main() async {
+  // Firebase core initialization
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  // Notifications initialization
+  await MainNotifications.initNotifications();
+
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
+
+
 
 
 
---
+++ lib/src/shared/notifications/main_notifications.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
+
+class MainNotifications {
+
+  static initNotifications() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings('@mipmap/ic_launcher');
+    const InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: (String? payload) async {
+        if (payload != null) {
+          debugPrint('notification payload: $payload');
+        }
+        // selectNotificationSubject.add(payload);
+      },
+      // backgroundHandler: notificationTapBackground,
+    );
+
+    _showNotification();
+  }
+
+  static Future<void> _showNotification() async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+    AndroidNotificationDetails('your channel id', 'your channel name',
+        channelDescription: 'your channel description',
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker');
+    const NotificationDetails platformChannelSpecifics =
+    NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        1, 'plain title', 'plain body', platformChannelSpecifics,
+        payload: 'item x');
+  }
+}
